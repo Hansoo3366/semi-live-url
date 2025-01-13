bba315824f3461d4a8aaa9c3e0577095c93185df
Hansoo <awaol200@naver.com>
2025-01-13 10:11:15 +0900

    Mon Jan 13 10:11:15 KST 2025 commit

--- git-auto-push.sh
+++ git-auto-push.sh
@@ -4,40 +4,40 @@ cd /volume1/web || exit
 
 #!/bin/bash
 
-date=`date`
+date=$(date)
 github_id="Hansoo3366"
-github_Token="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"
+github_Token="${GITHUB_TOKEN}"  # 환경 변수 사용
 github_Address="github.com/Hansoo3366/semi-live-url"
 logFile="/volume1/web/logs/push.log"
 SourceDir="/volume1/web"
 
-cd $SourceDir
+# 디렉토리 이동
+cd $SourceDir || { echo "Failed to change directory to $SourceDir"; exit 1; }
 
-echo "git add . ..." && echo "git add . ..." > $logFile 2>&1
-echo "`git add .`" >> $logFile 2>&1
-echo "" && echo "" >> $logFile 2>&1 && echo "==" >> $logFile 2>&1
+# Git 명령 실행
+{
+    echo "== Git 작업 시작 =="
 
-echo "git status" && echo "git status" >> $logFile 2>&1
-echo "`git status`" >> $logFile 2>&1
-echo "" && echo "" >> $logFile 2>&1 && echo "==" >> $logFile 2>&1
+    echo "git add ."
+    git add .
 
-echo "git commit -m $date commit" && echo "git commit -m $date commit" >> $logFile 2>&1
-echo "`git commit -m "$date commit"`" >> $logFile 2>&1
-echo "" && echo "" >> $logFile 2>&1 && echo "==" >> $logFile 2>&1
+    echo "git status"
+    git status
 
-echo "git push!" && echo "git push!" >> $logFile 2>&1
-git push https://$github_id:$github_Token@$github_Address >> $logFile 2>&1
+    echo "git commit -m \"$date commit\""
+    git commit -m "$date commit"
 
-sleep 2
+    echo "git push"
+    git push https://$github_id:$github_Token@$github_Address
 
-result="`awk '/Everything up-to-date/' $logFile 2>&1`"
-echo "$result"
+    echo "== Git 작업 완료 =="
+} >> $logFile 2>&1
 
-
-if [ -z "$result" ];then
-        echo "git push Success~!" && echo "git push Success~!" >> $logFile 2>&1
+# 결과 처리
+if [ $? -eq 0 ]; then
+    echo "git push Success~!" >> $logFile
 else
-        echo "!! git push ERROR! please check $logFile !!" && echo "!! git push ERROR! please check $logFile !!" >> $logFile 2>&1
+    echo "!! git push ERROR! please check $logFile !!" >> $logFile
 fi
 
 exit 0
